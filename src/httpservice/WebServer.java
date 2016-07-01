e8a19418d9b34cfe342a0423e176a051ee1775b2
cpu10859 <cpu10859@cpu10859-Local>
2016-07-01 14:58:43 +0700

    Merge origin/master
    Conflicts:
    	src/zp/web/controller/AjaxGetProductListController.java

--- src/httpservice/WebServer.java
+++ src/httpservice/WebServer.java
@@ -53,7 +53,7 @@ public class WebServer extends Thread {
 
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(dbg.edit.EntranceController.class, "/dbgclient/entrance");
-        handler.addServletWithMapping(dbg.edit.ConfirmController.class, "/dbgclient/confirm");
+        handler.addServletWithMapping(dbg.client.ConfirmController.class, "/dbgclient/confirm");
         handler.addServletWithMapping(dbg.client.ViewCacheController.class, "/dbgclient/viewCache");
         handler.addServletWithMapping(dbg.client.ViewTransHistoryController.class, "/dbgclient/viewTransHistory");
         handler.addServletWithMapping(dbg.client.GetTransStausController.class, "/dbgclient/getTransStatus");
